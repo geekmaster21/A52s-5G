acf60c882fcffc941df775442e0bc73a70628cbe
Maverick Leroux <78061003+geekmaster21@users.noreply.github.com>
2022-06-16 23:19:29 -0400

    Update for twrp Android12

--- omni_a52sxq.mk
+++ omni_a52sxq.mk
@@ -6,20 +6,19 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit from a52sxq device
 $(call inherit-product, device/samsung/a52sxq/device.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := a52sxq
-PRODUCT_NAME := omni_a52sxq
+PRODUCT_NAME := twrp_a52sxq
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A528B
 PRODUCT_MANUFACTURER := samsung
